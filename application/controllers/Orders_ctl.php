93e8b4bda3554963f1c27f744456c5bd208c0b7c
Shiva Pandey <t7pash00@students.oamk.fi>
2018-04-05 10:31:56 +0300

    Orders controller and model updated

--- application/controllers/Orders_ctl.php
+++ application/controllers/Orders_ctl.php
@@ -21,11 +21,92 @@ class Orders_ctl extends REST_Controller
     }
     public function orders_get()
     {
-
-    }
+        $products=$this->Orders_mdl->get_orders();
+        
+                $id = $this->get('id');
+        
+                // If the id parameter doesn't exist return all the users
+        
+                if ($id === NULL)
+                {
+                    // Check if the users data store contains users (in case the database result returns NULL)
+                    if ($orders)
+                    {
+                        // Set the response and exit
+                        $this->response($orders, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                    }
+                    else
+                    {
+                        // Set the response and exit
+                        $this->response([
+                            'status' => FALSE,
+                            'message' => 'No users were found'
+                        ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                    }
+                }
+        
+                // Find and return a single record for a particular user.
+        
+                $id = (int) $id;
+        
+                // Validate the id.
+                if ($id <= 0)
+                {
+                    // Invalid id, set the response and exit.
+                    $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+                }
+        
+                // Get the user from the array, using the id as key for retrieval.
+                // Usually a model is to be used for this.
+        
+                $product = NULL;
+        
+                if (!empty($orders))
+                {
+                    //Get the user from database
+                    $product=$this->Orders_mdl->get_product($id);
+                }
+        
+                if (!empty($orders))
+                {
+                    $this->set_response($orders, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                }
+                else
+                {
+                    $this->set_response([
+                        'status' => FALSE,
+                        'message' => 'orders could not be found'
+                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                }
+            }
+        
     public function orders_post()
-    {
 
+        {
+            // Add a new order
+            $add_data=array(
+              'SYSTEMUSERSID'=>$this->post('SYSTEMUSERSID'),
+              'PRODUCTSID'=>$this->post('PRODUCTSID'),
+              'ORDERDATE'=>$this->post('ORDERDATE'),
+              'ORDERSTATUS'=>$this->post('ORDERSTATUS'),
+              'PRODUCTRATE'=>$this->post('PRODUCTRATE'),
+              'ORDERQUANTITY'=>$this->post('ORDERQUANTITY'),
+              'ORDERPRICE'=>$this->post('ORDERPRICE')
+            );
+            $this->Orders_mdl->add_orders($add_data);
+            $message = [
+               
+              'SYSTEMUSERSID'=>$this->post('SYSTEMUSERSID'),
+              'PRODUCTSID'=>$this->post('PRODUCTSID'),
+              'ORDERDATE'=>$this->post('ORDERDATE'),
+              'ORDERSTATUS'=>$this->post('ORDERSTATUS'),
+              'PRODUCTRATE'=>$this->post('PRODUCTRATE'),
+              'ORDERQUANTITY'=>$this->post('ORDERQUANTITY'),
+              'ORDERPRICE'=>$this->post('ORDERPRICE'),
+                'message' => 'Added a resource'
+            ];
+            $this->set_response($message, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+        
     }
     public function orders_put()
     {
--- application/models/Orders_mdl.php
+++ application/models/Orders_mdl.php
@@ -4,5 +4,13 @@
  */
 class Orders_mdl extends CI_model
 {
-        
+    public function get_orders()
+    {
+        $this->db->select('*');
+        $this->db->from('orders');
+        return $this->db->get()->result_array();
+    }
+    function add_orders($add_data){
+        $this->db->insert('orders',$add_data); 
+     }
 }
