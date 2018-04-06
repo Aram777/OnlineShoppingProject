f15534d537f7d81610eaa09be513101e60b70249
Shiva Pandey <t7pash00@students.oamk.fi>
2018-04-06 14:05:36 +0300

    systemuser controller and model updated

--- application/controllers/Systemusers_ctl.php
+++ application/controllers/Systemusers_ctl.php
@@ -21,14 +21,125 @@ class Systemusers_ctl extends REST_Controller
     }
     public function systemusers_get()
     {
+        $systemusers=$this->Systemusers_mdl->get_systemusers();
+        
+                $SYSTEMUSERSID = $this->get('SYSTEMUSERSID');
+        
+                // If the id parameter doesn't exist return all the users
+        
+                if ($SYSTEMUSERSID === NULL)
+                {
+                    // Check if the users data store contains users (in case the database result returns NULL)
+                    if ($systemusers)
+                    {
+                        // Set the response and exit
+                        $this->response($systemusers, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                    }
+                    else
+                    {
+                        // Set the response and exit
+                        $this->response([
+                            'status' => FALSE,
+                            'message' => 'No systemusers were found'
+                        ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                    }
+                }
+        
+                // Find and return a single record for a particular user.
+        
+                $SYSTEMUSERSID = (int) $SYSTEMUSERSID;
+        
+                // Validate the id.
+                if ($SYSTEMUSERSID <= 0)
+                {
+                    // Invalid id, set the response and exit.
+                    $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+                }
+        
+                // Get the systemuser from the array, using the systemuserid as key for retrieval.
+                // Usually a model is to be used for this.
+        
+                $systemusers = NULL;
+        
+                if (!empty($systemusers))
+                {
+                    //Get the user from database
+                    $systemusers=$this->Systemusers_mdl->get_($SYSTEMUSERSID);
+                }
+        
+                if (!empty($systemusers))
+                {
+                    $this->set_response($systemusers, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+                }
+                else
+                {
+                    $this->set_response([
+                        'status' => FALSE,
+                        'message' => 'systemusers could not be found'
+                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                }
+            
 
     }
     public function systemusers_post()
     {
+        
+        // Add a new systemusers
+        $add_data=array(
+          
+          'USERFIRSTNAME'=>$this->post('USERFIRSTNAME'),
+          'USERLASTNAME'=>$this->post('USERLASTNAME'),
+          'USEREMAIL'=>$this->post('USEREMAIL'),
+          'USERTYPE'=>$this->post('USERTYPE'),
+          'USERSTATE'=>$this->post('USERSTATE'),
+          'USERADDRESS'=>$this->post('USERADDRESS'),
+          'USERPASS'=>$this->post('USERPASS')
+        );
+        $this->Systemusers_mdl->add_systemusers($add_data);
+        $message = [
+            
+            'USERFIRSTNAME'=>$this->post('USERFIRSTNAME'),
+            'USERLASTNAME'=>$this->post('USERLASTNAME'),
+            'USEREMAIL'=>$this->post('USEREMAIL'),
+            'USERTYPE'=>$this->post('USERTYPE'),
+            'USERSTATE'=>$this->post('USERSTATE'),
+            'USERADDRESS'=>$this->post('USERADDRESS'),
+            'USERPASS'=>$this->post('USERPASS'),
+            'message' => 'Added a resource'
+        ];
+        $this->set_response($message, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+    
+}
 
-    }
+    
     public function systemusers_put()
-    {
+    {// Update the systemusers
+
+
+        $SYSTEMUSERSID=$this->put('SYSTEMUSERSID');
+        $update_data=array(
+            'USERFIRSTNAME'=>$this->put('USERFIRSTNAME'),
+            'USERLASTNAME'=>$this->put('USERLASTNAME'),
+            'USEREMAIL'=>$this->put('USEREMAIL'),
+            'USERTYPE'=>$this->put('USERTYPE'),
+            'USERSTATE'=>$this->put('USERSTATE'),
+            'USERADDRESS'=>$this->put('USERADDRESS'),
+            'USERPASS'=>$this->put('USERPASS')
+          );
+
+        $this->Systemusers_mdl->update_systemusers($SYSTEMUSERSID, $update_data);
+        $message = [
+            'USERFIRSTNAME'=>$this->put('USERFIRSTNAME'),
+            'USERLASTNAME'=>$this->put('USERLASTNAME'),
+            'USEREMAIL'=>$this->put('USEREMAIL'),
+            'USERTYPE'=>$this->put('USERTYPE'),
+            'USERSTATE'=>$this->put('USERSTATE'),
+            'USERADDRESS'=>$this->put('USERADDRESS'),
+            'USERPASS'=>$this->put('USERPASS'),
+            'message' => 'Updates a resource'
+        ];
+        $this->set_response($message, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+    
     }
     public function systemusers_delete()
     {
--- application/models/Systemusers_mdl.php
+++ application/models/Systemusers_mdl.php
@@ -10,26 +10,26 @@ class Systemusers_mdl extends CI_model
         $this->db->from('systemusers');
         return $this->db->get()->result_array();
     }
-    public function get_systemuser($id)
+    public function get_systemuser($SYSTEMUSERSID)
     {
         $this->db->select('*');
         $this->db->from('systemusers');
-        $this->db->where('SYSTEMUSERSID', $id);
+        $this->db->where('SYSTEMUSERSID', $SYSTEMUSERSID);
         return $this->db->get()->result_array();
     }
     public function add_systemusers($add_data)
     {
         $this->db->insert('systemusers', $add_data);
     }
-    public function update_systemusers($id, $update_data)
+    public function update_systemusers($SYSTEMUSERSID, $update_data)
     {
-        $this->db->where('SYSTEMUSERSID', $id);
+        $this->db->where('SYSTEMUSERSID', $SYSTEMUSERSID);
         $this->db->update('systemusers', $update_data);
     }
 
-    public function delete_systemusers($id)
+    public function delete_systemusers($SYSTEMUSERSID)
     {
-        $this->db->where('SYSTEMUSERSID', $id);
+        $this->db->where('SYSTEMUSERSID', $SYSTEMUSERSID);
         $this->db->delete('systemusers');
     }
 
