845e9dce3908c7a9dd8fe1b5df09c5ae328b9917
Shiva Pandey <t7pash00@students.oamk.fi>
2018-04-06 10:28:59 +0300

    update pricecategory fixed in pricategory model and controllers

--- application/models/Pricecategory_mdl.php
+++ application/models/Pricecategory_mdl.php
@@ -12,5 +12,9 @@ class Pricecategory_mdl extends CI_model
       function add_Pricecategory($add_data){
         $this->db->insert('pricecategory',$add_data); 
      }
+     function update_pricecategory($PRICECATEGORYID, $update_data){
+        $this->db->where('PRICECATEGORYID',$PRICECATEGORYID);
+        $this->db->update('pricecategory',$update_data);
+      }
         
 }
