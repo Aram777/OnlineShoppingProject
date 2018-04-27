128e9b8b3432972a02f3545a96578d0008ba48a0
Suraj Pandeya <surajpandeya@Surajs-MacBook-Pro.local>
2018-04-27 09:44:14 +0300

    changes to js in shopcart

--- ShopCart/shiva.js
+++ ShopCart/shiva.js
@@ -2,19 +2,20 @@
 
 function makecart(){
     document.getElementById('shivadiv').innerHTML ='';
-    var url = 'http://localhost/OnlineShoppingProject/index.php/Orders_ctl/userorders/SystemUsersId/2';
+    var url = 'http://localhost/OnlineShoppingProject/index.php/Orders_ctl/userorders/SystemUsersId/';
+    alert(url);
     var xhttp = new XMLHttpRequest();
     xhttp.open('GET', url, true);
     var jsonData = '';
-var totprc=0
-    var data = '';
+//var totprc=0
+   // var data = '';
 
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             jsonData = JSON.parse(xhttp.responseText);
             for (x in jsonData) {
-              totprc+=$row['product_price']
-                data +=
+              //totprc+=$row['product_price']
+               // data +=
 
                 '<div class="Item">'+
                 '<div class="Itemimage">'+
